fb0f07f205a015184adf2095c1c61b90cf472e8d
Borja Valverde Irastorza <Borch97@users.noreply.github.com>
2024-06-07 11:00:06 +0200

    Create import.sql

---
+++ import.sql
@@ -0,0 +1,23 @@
+INSERT INTO configura_viaje (id, pregunta_en, opcion_1_en, opcion_2_en, opcion_3_en, opcion_4_en)
+
+VALUES
+
+(1, 'How do you travel?', 'In couple', 'In Family', 'With friends', 'Travelling alone');
+
+INSERT INTO configura_viaje (id, pregunta_en, opcion_1_en, opcion_2_en, opcion_3_en)
+
+VALUES
+
+(2, 'What are you interested in?', 'Beaches', 'Active tourism', 'Cultural activities');
+
+INSERT INTO configura_viaje (id, pregunta_en, opcion_1_en, opcion_2_en, opcion_3_en, opcion_4_en)
+
+VALUES
+
+(3, 'How much time do you have?', 'One day', 'Two days', 'One week', 'More than one week');
+
+INSERT INTO configura_viaje (id, pregunta_en, opcion_1_en, opcion_2_en, opcion_3_en, opcion_4_en)
+
+VALUES
+
+(4, 'How are you going to move?', 'By taxi', 'By bus', 'In maritime transport', 'By rental car');
