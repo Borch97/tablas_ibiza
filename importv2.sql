c97de81bf0a5cd0e4b10923452912ec3660c0484
Borja Valverde Irastorza <Borch97@users.noreply.github.com>
2024-06-07 11:05:55 +0200

    Create importv2.sql

---
+++ importv2.sql
@@ -0,0 +1,155 @@
+ALTER TABLE tts ADD COLUMN tts_en TEXT,
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(2, 'Welcome to Ibiza Smart Island! In this application you can find all the tourist information of the island of Ibiza and make your stay more enjoyable. It is located in the main menu of the application, here you can access the different sections. Tourist information. In this section you can access the following sections: Our villages; with information on all the villages on the island, the information can be viewed in map format and in list format, and you can access the files of each of the villages. Our beaches; with information on all the beaches on the island, the information can be viewed in map format and in list format, and you can access the data sheets for each of the beaches and see their occupancy, water quality and air quality live. Places of interest; with information on all the points of interest on the island, the information can be viewed in map format and in list format, and you can access the files for each of the places of interest. Gastronomy; in this section you can view information on the typical gastronomy of the island. Music and nightlife; with information on all the island\'s nightlife venues, the information can be viewed in map format and in list format, and you can access the files for each of the island\'s nightlife venues. Markets and crafts; with information on all the markets available on the island, the information can be viewed in map format and in list format, and you can access the information sheets for each of the markets. Configure your trip. In this section you will find the possibility of personalising your trip by answering some questions, the application will recommend options based on your answers. These results can be viewed in the ‘See my trip’ section. Agenda. In this section you can view the events available on the island. By clicking on them you can access the website with more information about the resource. Public transport. In this section you can access the following sections: Getting around by bus; with information on the arrival time of the city buses at each of the available stops. You can also consult all the routes in map format. The app will offer you the routes closest to your position. Taxi information; in this section you can view the location of all the taxi ranks available on the island, displaying it in map format and in list format, with all the data associated with each of the taxi ranks. Maritime transport; in this section you can view the location of all the maritime transport companies available on the island, which can be viewed in map format and in list format, and you can view all the data associated with each of the companies. Search for parking. In this section you can access information on all the public car parks available. These data can be viewed in map format and in list format, and the occupancy of the car parks can be viewed in real time. In the lower menu there is a management menu with the elements marked as favourites and the settings available in the application, such as accessibility, languages, electronic headquarters and credits.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(1, 'Welcome to Ibiza Smart Island! In this application you can find all the tourist information of the island of Ibiza and make your stay more enjoyable. It is located in the main menu of the application, here you can access the different sections. Tourist information. In this section you can access the following sections: Our villages; with information on all the villages on the island, the information can be viewed in map format and in list format, and you can access the files of each of the villages. Our beaches; with information on all the beaches on the island, the information can be viewed in map format and in list format, and you can access the data sheets for each of the beaches and see their occupancy, water quality and air quality live. Places of interest; with information on all the points of interest on the island, the information can be viewed in map format and in list format, and you can access the files for each of the places of interest. Gastronomy; in this section you can view information on the typical gastronomy of the island. Music and nightlife; with information on all the island\'s nightlife venues. The information can be viewed in map format and in list format, and you can access the files for each of the island\'s nightlife venues. Markets and crafts; with information on all the markets available on the island, the information can be viewed in map format and in list format, and you can access the files for each of the markets. Configure your trip. In this section you will find the possibility of personalising your trip by answering some questions, the
+
+application will recommend options based on your answers. These results can be viewed in the ‘See my trip’ section. Agenda. In this section you can view the events available on the island. By clicking on them you can access the website with more information about the resource. Public transport. In this section you can access the following sections: Getting around by bus; with information on the arrival time of the city buses at each of the available stops. You can also consult all the routes in map format. The app will offer you the routes closest to your position. Taxi information; in this section you can view the location of all the taxi ranks available on the island, displaying it in map format and in list format, with all the data associated with each of the taxi ranks. Maritime transport; in this section you can view the location of all the maritime transport companies available on the island, which can be viewed in map format and in list format, and you can view all the data associated with each of the companies. Search for parking. In this section you can access information on all available public car parks. This data can be viewed in map format and in list format, and the occupancy of the car parks can be viewed in real time. In the lower menu there is a management menu with the elements marked as favourites and the settings available in the application such as accessibility, languages and credits.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(3, 'Our Villages. List. In this section you can see all the villages on the island in list format with a search engine. By clicking on them you will be able to access the file of each one of them. At the top you have a selector menu where you can view the villages in map format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(4, 'Our villages. Map. In this section you can see all the villages on the island in map format. By clicking on them you will be able to access the file of each one. At the top you have a selector menu where you can view the villages in list format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(5, 'Our beaches. List. In this section you can see all the beaches on the island in list format with an occupancy indicator and a search engine. By clicking on them you will be able to access each one. At the top you have a selector menu where you can access to view the beaches in map format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(6, 'Our beaches. Map. In this section you can see all the beaches on the island in map format with an occupancy indicator. By clicking on them you will be able to access each one. At the top you have a selector menu where you can view the beaches in list format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(7, 'Places of interest. List. In this section you can see all the places of interest on the island in list format with a search engine. By clicking on them you will be able to access the file for each one. At the top you have a selector menu where you can view the places of interest in map format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(8, 'Places of interest. Map. In this section you can see all the places of interest on the island in map format. By clicking on them you will be able to access the file for each one. At the top you have a selector menu where you can view the places of interest in list format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(9, 'Ibiza seduces with its landscapes, its climate, its people and also because it offers the best gastronomy on the beach, in historic places, in the mindexdle of the countrysindexe or under a starry sky. Enjoy the luxury of eating natural products, at their exact point of ripeness, and remember the flavours of your childhood. Enjoy the peace and the atmosphere of freedom of the island, just when you are served a seafood rice dish by the sea or when you taste the freshest fish, with exquisite species such as grouper, tuna, John Dory or raor. Ibiza is a journey of sensations. The excellent quality of the raw ingredients is mixed with tradition in recipes that have come down to us with the same originality as always: sofrit pagès, matanzas rice, guisat de peix, greixonera or flaó are centuries-old dishes that transmit a culture and a way of life where the meeting of families and friends, respect for the legacy of the ancestors, the preservation of the natural environment and the products of the land and also something as intangible as enjoying and being happy are essential. Let yourself be surprised by the local wine, virgin olive oil or local bread, delicacies that are on every table, accompanying magical moments in which eating means taking care of yourself, learning new things, sharing and smiling. WINE. Since the 8th century BC, the influence of the Phoenicians, Greeks and Romans has generated an important wine-growing tradition on the islands. The smallholding agrarian economy managed to maintain the cultivation of vines and the art of winemaking for personal consumption, a tradition passed down from father to son until the present day. In 1990 the wine trade was developed in the Pitiusas which, in a short period of time, has become one of the best wines of Mediterranean essence. These are very limited productions where quality prevails over quantity and from which wines with deep, provocative and balsamic aromas are obtained, with a smooth but structured body, indexeal to accompany any dish of the local gastronomy. The native varieties are Monastrel and Garnacha in reds, while in whites the Malvasía variety stands out. Ibizan wines are made and produced in the areas of Sant Mateu, Buscastell and Sant Josep. Vins de la Terra is the Geographical Indication that covers the entire production of Ibiza wine according to European regulations. OLIVE OIL. Olive oil has been produced on the island of Ibiza using traditional methods since ancient times. The existence of numerous oil mills (‘trulls’) and olive trees scattered among carob and almond trees testifies to the importance of this production, which was basically aimed at covering the needs of the local market. Nowadays there are two oil mills with modern installations that enable them to comply with current health legislation and obtain an excellent quality extra-virgin olive oil. The most common olive varieties in Ibiza are empeltre (traditional variety, also known as ibicencan), arbequina and picual. The first two give fruity oils which, with a little picual, increase their shelf life. The latter gives oils with a stronger taste. Oli d\'e is the collective guarantee brand created by the island\'s oil producers. IBICENCAN HERBS. Ibizan herbs are an aniseed-flavoured spirit obtained basically by extracting the aromas of various local plants such as rosemary, thyme, mint, peppermint, juniper, sage, fennel, lavender, lemon verbena and lemon and orange tree leaves. They are a very popular drink in Ibiza and are part of the tradition of many homes that have always made them by hand, each family following their own secret recipe on many occasions. Hierbas Ibicencas is the Geographical Indication that has protected this drink since 1997. FLAÓ. In the past, each typical Ibiza dish was eaten at a certain time of year, and in the case of flaó, this is associated with Easter (it was traditionally prepared for Easter Sunday). Nowadays it can be eaten at any time of the year. Flaó is a round cake made with eggs, fresh sheep\'s or sheep\'s and goat\'s cheese and mint leaves, which give it a very particular flavour reminiscent of Arabic, although its origins are mediaeval. Flaó d\'e is the Guarantee Mark that includes all the local producers who have registered. IBIZIAN LAMB. The meat of lamb born on Ibiza and reared on the island according to traditional practices (extensive free-grazing systems and vegetable feed) is certified by the Anyell d\'e Guarantee Mark. This meat is sold in authorised retail establishments and the product is clearly differentiated from other meats.
+
+CERTIFIED HONEY FROM IBIZA. The honey produced on Ibiza is only harvested twice a year. In summer, multi-flower honey is harvested.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(10, 'Music and night. List. In this section you can see all the nightlife venues on the island in list format with a search engine. By clicking on them you will be able to access each one of them. At the top you have a selector menu where you can view the nightlife venues in map format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(11, 'Music and night. Map of the island. In this section you can see all the nightlife venues on the island in map format. By clicking on them you will be able to access each one of them. At the top you have a selector menu where you can view the nightlife venues in list format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(12, 'Markets and crafts. List. In this section you can see all the markets and places dedicated to crafts on the island in list format with a search engine. By clicking on them you will be able to access each one''s file. At the top you have a selector menu where you can view the markets and places dedicated to crafts in map format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(13, 'Markets and handicrafts. Map of the island. In this section you can see all the markets and places dedicated to crafts on the island in map format. By clicking on them you will be able to access each one. At the top you have a selector menu where you can access to view the markets and places dedicated to crafts in list format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(14, 'In the Configure your trip section you will find two options: Customise your trip and View my trip.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(15, 'In the agenda section you can find the events that will take place on the island in the near future.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(16, 'In the Public transport section you can find all the information you need to get around the island, divindexed into three sections: getting around by bus, taxi information and sea transport.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(17, 'Getting around by bus. List. In this section you can see the bus lines of the island in list format with a search engine. By clicking on them you can see the stops along the route and the time at which the bus runs. At the top you have a selector menu where you can view the bus lines in map format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(18, 'Getting around by bus. Bus map. In this section you can see the bus lines of the island in map format. By clicking on them you can see the route. At the top you have a selector menu where you can view the bus lines in list format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(19, 'Taxis. List. In this section you can see all the taxi ranks on the island in list format with a drop-down search engine to find the ranks by municipality. Next to each rank there is a location icon to show you how to get to the rank. By clicking on a rank you will be able to see more information about it, including the address, telephone number and website. At the top you have a selector menu where you can access to view the taxi ranks in map format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(20, 'Taxis. Taxi map. In this section you can see all the taxi ranks on the island in map format. By clicking on them you can see how to get there. At the top you have a selector menu where you can view the taxi ranks in list format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(21, 'Maritime transport. List. In this section you can view the island''s maritime transport in list format with a drop-down search engine to find transport by municipality. Next to each transport there is a location icon to see how to get to the stop. By clicking on a stop you will be able to expand its information, displaying the address, telephone number and website. At the top of the page there is a selector menu where you can view the maritime transport in map format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(22, 'Maritime transport. Map. In this section you can see the maritime transport of the island in map format. By clicking on them you can see how to get there. In the upper part you have a selector menu where you can access to visualize the maritime transports in list format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(23, 'In the section Find parking you can find information about public car parks on the island.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(24, 'Public car parks. List. In this section you can see the public car parks on the island in list format with a search engine. Next to each car park there is a location icon to show you how to get to the stop. By clicking on them you can see the total number of spaces and the number of free spaces. At the top you have a selector menu where you can view the public car parks in map format.');
+
+INSERT INTO tts (index, tts_en)
+
+VALUES
+
+(25, 'Public car parks. Map. In this section you can see the public car parks on the island in map format. By clicking on them you can see how to get there. At the top you have a selector menu where you can view the public car parks in list format.');
