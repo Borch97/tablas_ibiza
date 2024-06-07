42712940211e6076df1618abd2f1f3c18699d539
Borja Valverde Irastorza <Borch97@users.noreply.github.com>
2024-06-07 11:03:44 +0200

    Update import.sql

--- import.sql
+++ import.sql
@@ -1,22 +1,22 @@
-INSERT INTO configura_viaje (id, pregunta_en, opcion_1_en, opcion_2_en, opcion_3_en, opcion_4_en)
+INSERT INTO configura_viaje (index, pregunta_en, opcion_1_en, opcion_2_en, opcion_3_en, opcion_4_en)
 
 VALUES
 
 (1, 'How do you travel?', 'In couple', 'In Family', 'With friends', 'Travelling alone');
 
-INSERT INTO configura_viaje (id, pregunta_en, opcion_1_en, opcion_2_en, opcion_3_en)
+INSERT INTO configura_viaje (index, pregunta_en, opcion_1_en, opcion_2_en, opcion_3_en)
 
 VALUES
 
 (2, 'What are you interested in?', 'Beaches', 'Active tourism', 'Cultural activities');
 
-INSERT INTO configura_viaje (id, pregunta_en, opcion_1_en, opcion_2_en, opcion_3_en, opcion_4_en)
+INSERT INTO configura_viaje (index, pregunta_en, opcion_1_en, opcion_2_en, opcion_3_en, opcion_4_en)
 
 VALUES
 
 (3, 'How much time do you have?', 'One day', 'Two days', 'One week', 'More than one week');
 
-INSERT INTO configura_viaje (id, pregunta_en, opcion_1_en, opcion_2_en, opcion_3_en, opcion_4_en)
+INSERT INTO configura_viaje (index, pregunta_en, opcion_1_en, opcion_2_en, opcion_3_en, opcion_4_en)
 
 VALUES
 
